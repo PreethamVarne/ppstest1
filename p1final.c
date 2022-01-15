5e3180d534cd599cf045025f014008005a319c93
Preetham Varne <97089332+PreethamVarne@users.noreply.github.com>
2022-01-15 07:30:56 +0000

    removed return statement

---
+++ p1final.c
@@ -0,0 +1,17 @@
+//add two nmbers
+
+#include<stdio.h>
+void add()
+{
+  int num1,num2;
+  float sum;
+  printf("enter two numbers\n");
+  scanf("%d%d",&num1,&num2);
+  sum = num1 + num2;
+  printf("addition of %d + %d is =%f \n",num1,num2,sum);
+}
+int main()
+{
+  add();
+  return 0;
+}
